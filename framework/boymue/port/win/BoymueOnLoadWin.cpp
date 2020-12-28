580a09beb99644816d2272c2864a8e53b9af8101
yanbo.boyia <2512854007@qq.com>
2020-12-28 10:01:53 +0800

    [feat]: add test demo

--- framework/boymue/port/win/BoymueOnLoadWin.cpp
+++ framework/boymue/port/win/BoymueOnLoadWin.cpp
@@ -3,6 +3,7 @@
 #include "SkBitmap.h"
 #include "SkString.h"
 #include "SkCanvas.h"
+#include "SkSurface.h"
 #include <stdio.h>
 
 // Copyright Boymue Authors. All rights reserved.
@@ -35,6 +36,7 @@ public:
 
     virtual void run() 
     {
+        /*
         LPVOID pBits = NULL;
         m_bitmap = createGDIBitmap(360, 640, &pBits);
         m_skbitmap = new SkBitmap();
@@ -45,6 +47,7 @@ public:
 
         SkCanvas* canvas = new SkCanvas(*m_skbitmap);
         SkPaint paint;
+        paint.setXfermodeMode(SkXfermode::kClear_Mode);
         paint.setColor(SK_ColorRED);
         paint.setTextSize(SkIntToScalar(30));
         paint.setAntiAlias(true);
@@ -56,8 +59,81 @@ public:
             10, fontHeight,
             paint);
 
+        
+
+        HDC hdc = GetDC(m_hwnd);
+
+        HDC comDC = ::CreateCompatibleDC(hdc);
+        SelectObject(comDC, m_bitmap);
+        BitBlt(comDC, 0, 0, 360, 640, hdc, 0, 0, SRCCOPY);
+
+        ReleaseDC(m_hwnd, hdc);
+        DeleteDC(comDC);*/
+        HDC hdc = GetDC(m_hwnd);
+        RECT rt;
+        GetClientRect(m_hwnd, &rt);
+        int bmpw = rt.right - rt.left;
+        int bmph = rt.bottom - rt.top;
+
+        const size_t bmpSize = sizeof(BITMAPINFOHEADER) + bmpw * bmph * sizeof(uint32_t);
+        BITMAPINFO* bmpInfo = (BITMAPINFO*)new BYTE[bmpSize]();
+        bmpInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bmpInfo->bmiHeader.biWidth = bmpw;
+
+        bmpInfo->bmiHeader.biHeight = -bmph;
+
+        bmpInfo->bmiHeader.biPlanes = 1;
+        bmpInfo->bmiHeader.biBitCount = 32;
+        bmpInfo->bmiHeader.biCompression = BI_RGB;
+        void* pixels = bmpInfo->bmiColors;
+
+        SkImageInfo info = SkImageInfo::Make(bmpw, bmph,
+            kBGRA_8888_SkColorType, kPremul_SkAlphaType);
+
+        SkSurface* surface = SkSurface::NewRasterDirect(info, pixels, bmpw * sizeof(uint32_t));
+        SkCanvas* canvas = surface->getCanvas();
+
+        canvas->clear(SK_ColorWHITE);
+
+        Draw(canvas, bmpw, bmph);
+
+        StretchDIBits(hdc, 0, 0, bmpw, bmph,
+            0, 0, bmpw, bmph,
+            pixels, bmpInfo,
+            DIB_RGB_COLORS, SRCCOPY);
+
+        delete[] bmpInfo;
         delete this;
     }
+
+    void Draw(SkCanvas* canvas, int w, int h) {
+
+        SkPaint paint;
+        paint.setStrokeWidth(1);
+        paint.setARGB(0xff, 0xff, 0, 0);
+
+        for (int i = 0; i < h; i++)
+        {
+            canvas->drawPoint(0, i, paint);
+        }
+
+        for (int i = w; i > 0; i--)
+        {
+            canvas->drawPoint(i - 1, h - 1, paint);
+        }
+
+        SkPaint textpaint;
+        textpaint.reset();
+        textpaint.setTextSize(16);
+        textpaint.setColor(SkColor(0xffff0000));
+        textpaint.setAntiAlias(true);
+        SkString string("Hello World");
+
+        canvas->drawText(&string, string.size(), 1, 16, textpaint);
+
+        canvas->flush();
+    }
+
 private:
     HWND m_hwnd;
     HBITMAP m_bitmap;
